ab3c10f3efd71219ce21663f9804dbcdd86d9471
Nihaar Thakkar <nihaar.thakkar@gmail.com>
2026-04-11 07:15:06 -0400

    Null bet score fix

--- award_scoring.py
+++ award_scoring.py
@@ -1,11 +1,12 @@
 #Function to calculate bet score based on multiplier, success, and bet amount 
 from collections import Counter
 import pandas as pd
+import numpy as np
 
 def get_bet_score(cancel, multiplier, success, bet):
 
     score = {k: (not cancel[k]) * ((multiplier[k] * success[k] * bet[k]) - bet[k]) for k in multiplier}
-    sum_score = sum(score.values())
+    sum_score = sum(0 if np.isnan(v) else v for v in score.values())
     return sum_score
 
 def get_bet_metrics(p, metric, df):
